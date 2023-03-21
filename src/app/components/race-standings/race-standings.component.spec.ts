1b5d705483a436e2930ab16a867f727d1ee05819
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-03-21 12:37:43 -0400

    TEST: race standings component(bonus)

--- src/app/components/race-standings/race-standings.component.spec.ts
+++ src/app/components/race-standings/race-standings.component.spec.ts
@@ -1,23 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RaceStandingsComponent } from './race-standings.component';
+import { RaceService } from '../../services/race.service';
+import { of } from 'rxjs';
 
 describe('RaceStandingsComponent', () => {
   let component: RaceStandingsComponent;
   let fixture: ComponentFixture<RaceStandingsComponent>;
+  let raceService: RaceService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RaceStandingsComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [RaceStandingsComponent],
+      providers: [RaceService, {
+        provide: ActivatedRoute,
+        useValue: {
+          params: of({ season: '2023', round: '1' }),
+        },
+      }],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(RaceStandingsComponent);
     component = fixture.componentInstance;
+    raceService = TestBed.inject(RaceService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load driver standings on init', () => {
+    spyOn(component, 'loadDriverStandings');
+    component.ngOnInit();
+    expect(component.loadDriverStandings).toHaveBeenCalled();
+  });
+
+  it('should call service to get driver standings when loading standings', () => {
+    spyOn(raceService, 'getDriverStandings').and.callThrough();
+    component.loadDriverStandings();
+    expect(raceService.getDriverStandings).toHaveBeenCalled();
+  });
+
+  it('should load driver standings on form submit', () => {
+    spyOn(component, 'loadDriverStandings');
+    component.onFormSubmit('2023', '1');
+    expect(component.loadDriverStandings).toHaveBeenCalled();
+  });
+
+  it('should load driver standings on season change', () => {
+    spyOn(component, 'loadDriverStandings');
+    component.onSeasonChange('2023', '1');
+    expect(component.loadDriverStandings).toHaveBeenCalled();
+  });
+
 });
