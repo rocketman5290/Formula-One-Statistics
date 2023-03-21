7c2e6c14cd8f129071fee46fdc4d1b84906b2428
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-03-21 12:35:42 -0400

    TEST: circut list component

--- src/app/components/circuit-list/circuit-list.component.spec.ts
+++ src/app/components/circuit-list/circuit-list.component.spec.ts
@@ -1,23 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CircuitListComponent } from './circuit-list.component';
+import { CircuitService } from '../../services/circuit.service';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 describe('CircuitListComponent', () => {
   let component: CircuitListComponent;
   let fixture: ComponentFixture<CircuitListComponent>;
+  let circuitService: CircuitService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CircuitListComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule, NgbPaginationModule],
+      declarations: [CircuitListComponent],
+      providers: [CircuitService],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CircuitListComponent);
     component = fixture.componentInstance;
+    circuitService = TestBed.inject(CircuitService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load circuits on init', () => {
+    spyOn(component, 'loadCircuits');
+    component.ngOnInit();
+    expect(component.loadCircuits).toHaveBeenCalled();
+  });
+
+  it('should call service to get circuits when loading circuits', () => {
+    spyOn(circuitService, 'getCircuits').and.callThrough();
+    component.loadCircuits();
+    expect(circuitService.getCircuits).toHaveBeenCalled();
+  });
+
+  it('should change page and load circuits on page change', () => {
+    spyOn(component, 'loadCircuits');
+    const newPage = 2;
+    component.onPageChange(newPage);
+    expect(component.currentPage).toBe(newPage);
+    expect(component.loadCircuits).toHaveBeenCalled();
   });
 });
