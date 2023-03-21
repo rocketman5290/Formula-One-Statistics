6c4f945982a905c88ffbba7a04416911270a2270
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-03-21 11:41:07 -0400

    TEST: circuit.service specs

--- src/app/services/circuit.service.spec.ts
+++ src/app/services/circuit.service.spec.ts
@@ -1,16 +1,61 @@
 import { TestBed } from '@angular/core/testing';
-
-import { CircuitService } from './circuit.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { CircuitService, CircuitsResponse } from './circuit.service';
 
 describe('CircuitService', () => {
   let service: CircuitService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CircuitService],
+    });
     service = TestBed.inject(CircuitService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch circuits with correct parameters', () => {
+    const mockCircuitsResponse: CircuitsResponse = {
+      circuits: [
+        {
+          circuitId: 'test_circuit',
+          circuitName: 'Test Circuit',
+          Location: {
+            locality: 'Test Locality',
+            country: 'Test Country',
+          },
+        },
+      ],
+      total: 1,
+    };
+
+    const offset = 0;
+    const limit = 1;
+
+    service.getCircuits(offset, limit).subscribe((data) => {
+      expect(data).toEqual(mockCircuitsResponse);
+    });
+
+    const req = httpMock.expectOne(
+      `https://ergast.com/api/f1/circuits.json?offset=${offset}&limit=${limit}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush({
+      MRData: {
+        CircuitTable: {
+          Circuits: mockCircuitsResponse.circuits,
+        },
+        total: mockCircuitsResponse.total,
+      },
+    });
   });
 });
