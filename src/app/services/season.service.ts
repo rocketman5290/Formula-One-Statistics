17a10f3ab53413eb7144f76991db8f0a7fd1323b
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-03-21 16:26:04 -0400

    ADD: error handling to season service

--- src/app/services/season.service.ts
+++ src/app/services/season.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,16 +14,47 @@ export class SeasonService {
   getDriversBySeason(season: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/${season}/drivers.json`)
       .pipe(
-        map((response) => ({
-          drivers: response.MRData.DriverTable.Drivers
-        }))
+        map((response) => {
+          if (
+            response &&
+            response.MRData &&
+            response.MRData.DriverTable &&
+            response.MRData.DriverTable.Drivers
+          ) {
+            return {
+              drivers: response.MRData.DriverTable.Drivers
+            };
+          } else {
+            console.error('Unexpected API response:', response);
+            return { drivers: [] };
+          }
+        }),
+        catchError((error) => {
+          console.error('Error loading drivers by season:', error);
+          return of({ drivers: [] });
+        })
       );
   }
 
   getRacesBySeason(season: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/${season}.json`).pipe(
-      map((response) => response.MRData.RaceTable.Races)
+      map((response) => {
+        if (
+          response &&
+          response.MRData &&
+          response.MRData.RaceTable &&
+          response.MRData.RaceTable.Races
+        ) {
+          return response.MRData.RaceTable.Races;
+        } else {
+          console.error('Unexpected API response:', response);
+          return [];
+        }
+      }),
+      catchError((error) => {
+        console.error('Error loading races by season:', error);
+        return of([]);
+      })
     );
   }
-
 }
