977baee74268e023b6265aff775031d0ef78f537
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-03-21 03:19:01 -0400

    ADD: season-drivers.component implementation

--- src/app/components/season-drivers/season-drivers.component.ts
+++ src/app/components/season-drivers/season-drivers.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SeasonService } from '../../services/season.service';
 
 @Component({
   selector: 'app-season-drivers',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./season-drivers.component.css']
 })
 export class SeasonDriversComponent {
+  drivers: any[] = [];
+  selectedSeason: number = new Date().getFullYear();
 
+  constructor(private seasonService: SeasonService) {}
+
+  ngOnInit(): void {
+    this.loadDrivers();
+  }
+
+  loadDrivers(): void {
+    this.seasonService
+      .getDriversBySeason(this.selectedSeason)
+      .subscribe((data) => {
+        this.drivers = data;
+      });
+  }
+
+  onSeasonChange(season: number): void {
+    this.selectedSeason = season;
+    this.loadDrivers();
+  }
+  
 }
