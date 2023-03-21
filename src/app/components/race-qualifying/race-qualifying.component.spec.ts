95be6927fb90149199c1b98e97532854c1a44a49
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-03-21 12:37:25 -0400

    TEST: race qualifying component

--- src/app/components/race-qualifying/race-qualifying.component.spec.ts
+++ src/app/components/race-qualifying/race-qualifying.component.spec.ts
@@ -1,23 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RaceQualifyingComponent } from './race-qualifying.component';
+import { RaceService } from '../../services/race.service';
+import { of } from 'rxjs';
 
 describe('RaceQualifyingComponent', () => {
   let component: RaceQualifyingComponent;
   let fixture: ComponentFixture<RaceQualifyingComponent>;
+  let raceService: RaceService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RaceQualifyingComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [RaceQualifyingComponent],
+      providers: [RaceService, {
+        provide: ActivatedRoute,
+        useValue: {
+          params: of({ season: '2023', round: '1' }),
+        },
+      }],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(RaceQualifyingComponent);
     component = fixture.componentInstance;
+    raceService = TestBed.inject(RaceService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load qualifying results on init', () => {
+    spyOn(component, 'loadQualifyingResults');
+    component.ngOnInit();
+    expect(component.loadQualifyingResults).toHaveBeenCalled();
+  });
+
+  it('should call service to get qualifying results when loading results', () => {
+    spyOn(raceService, 'getQualifyingResults').and.callThrough();
+    component.loadQualifyingResults();
+    expect(raceService.getQualifyingResults).toHaveBeenCalled();
+  });
+
+  it('should load qualifying results on form submit', () => {
+    spyOn(component, 'loadQualifyingResults');
+    component.onFormSubmit('2023', '1');
+    expect(component.loadQualifyingResults).toHaveBeenCalled();
+  });
+
+  it('should load qualifying results on season change', () => {
+    spyOn(component, 'loadQualifyingResults');
+    component.onSeasonChange('2023', '1');
+    expect(component.loadQualifyingResults).toHaveBeenCalled();
+  });
+
 });
