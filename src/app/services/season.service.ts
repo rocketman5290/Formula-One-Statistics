d3d4abcc28037f50702e1593b22c1e6819ef83e7
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-03-21 03:12:58 -0400

    ADD: getDriversBySeason to season service

--- src/app/services/season.service.ts
+++ src/app/services/season.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class SeasonService {
+  private apiUrl = 'https://ergast.com/api/f1';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDriversBySeason(season: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${season}/drivers.json`)
+      .pipe(
+        map((response) => ({
+          drivers: response.MRData.DriverTable.Drivers
+        }))
+      );
+  }
+
+  
 }
