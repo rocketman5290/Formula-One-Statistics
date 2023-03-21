eab1a5a7f6c685a84786ce6b25c14ea8036b5fce
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-03-21 15:42:05 -0400

    ADD: Basic error handling for circuits-list component

--- src/app/components/circuit-list/circuit-list.component.ts
+++ src/app/components/circuit-list/circuit-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Circuit, CircuitService } from '../../services/circuit.service';
+import { catchError } from 'rxjs';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-circuit-list',
@@ -11,6 +13,7 @@ export class CircuitListComponent implements OnInit {
   total: number = 0;
   currentPage: number = 1;
   itemsPerPage: number = 10;
+  errorMessage: string | null = null;
 
   constructor(private circuitService: CircuitService) {}
 
@@ -21,6 +24,13 @@ export class CircuitListComponent implements OnInit {
   loadCircuits(): void {
     this.circuitService
       .getCircuits((this.currentPage - 1) * this.itemsPerPage, this.itemsPerPage)
+      .pipe(
+        catchError((err) => {
+          console.error("Error loading circuits: ", err);
+          this.errorMessage = "An error occurred while loading circuits. Please try again later.";
+          return of({ circuits: [], total: 0 });
+        })
+      )
       .subscribe((data) => {
         this.circuits = data.circuits;
         this.total = data.total;
