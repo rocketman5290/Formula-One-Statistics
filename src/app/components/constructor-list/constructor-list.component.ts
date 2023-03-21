cb48dc7f3f0a7ad231a0e1781b99b524dd2376f3
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2023-03-21 15:44:48 -0400

    ADD: Basic error handling for constructor list component

--- src/app/components/constructor-list/constructor-list.component.ts
+++ src/app/components/constructor-list/constructor-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Constructor, ConstructorService } from '../../services/constructor.service';
-
+import { catchError } from 'rxjs';
+import { of } from 'rxjs';
 @Component({
   selector: 'app-constructor-list',
   templateUrl: './constructor-list.component.html',
@@ -11,6 +12,7 @@ export class ConstructorListComponent implements OnInit {
   total: number = 0;
   currentPage: number = 1;
   itemsPerPage: number = 10;
+  errorMessage: string | null = null;
 
   constructor(private constructorService: ConstructorService) {}
 
@@ -21,6 +23,13 @@ export class ConstructorListComponent implements OnInit {
   loadConstructors(): void {
     this.constructorService
       .getConstructors((this.currentPage - 1) * this.itemsPerPage, this.itemsPerPage)
+      .pipe(
+        catchError((err) => {
+          console.error("Error loading constructors: ", err);
+          this.errorMessage = "An error occurred while loading constructors. Please try again later.";
+          return of({ constructors: [], total: 0 });
+        })
+      )
       .subscribe((data) => {
         this.constructors = data.constructors;
         this.total = data.total;
